0cf87bbb7b387b6130c448f58a93a6dca0f1cdbc
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-10-01 18:16:04 +0530

    stage 6

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,21 @@ import com.dampcake.bencode.Bencode;
 import com.dampcake.bencode.Type;
 import com.google.gson.Gson;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Map;
 
 public class Main {
   private static final Gson gson = new Gson();
   private static final Bencode bencode = new Bencode();
+  private static final Bencode bencodeForTorrentFile = new Bencode(true);
 
   public static void main(String[] args) throws Exception {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -19,7 +27,13 @@ public class Main {
       String bencodedValue = args[1];
       Object decoded;
       try {
+//        final Type type = bencode.type(bencodedValue.getBytes());
+//        System.out.println("type = " + type.toString());
+
         decoded = bencode.decode(bencodedValue.getBytes(), bencode.type(bencodedValue.getBytes()));
+
+//        System.out.println("decoded = " + decoded.getClass());
+
       } catch (RuntimeException e) {
         System.out.println(e.getMessage());
         return;
@@ -29,19 +43,47 @@ public class Main {
     } else if ("info".equals(command)) {
       final byte[] torrentFile = Files.readAllBytes(Paths.get(args[1]));
 
-      final Map<String, Object> torrentFileDecoded = bencode.decode(torrentFile, Type.DICTIONARY);
+      final Map<String, Object> torrentFileDecoded = bencodeForTorrentFile.decode(torrentFile, Type.DICTIONARY);
 
-      final Object url = torrentFileDecoded.get("announce");
-      System.out.printf("Tracker URL: %s\n", url);
+      final ByteBuffer announce = (ByteBuffer) torrentFileDecoded.get("announce");
+      System.out.printf("Tracker URL: %s\n", new String(announce.array()));
 
       final Map<String, Object> info = (Map<String, Object>) torrentFileDecoded.get("info");
 
       System.out.printf("Length: %s\n", info.get("length"));
 
+      final String infoHash = getInfoHash(torrentFileDecoded);
+      System.out.printf("Info Hash: %s\n", infoHash);
+
     } else {
       System.out.println("Unknown command: " + command);
     }
 
+  }
+
+  static void printInfoDict(Map<String, Object> info) throws IOException {
+    for (Map.Entry<String, Object> kv : info.entrySet()) {
+      final String key = kv.getKey();
+      final Object value = kv.getValue();
+      try (
+          ByteArrayOutputStream bos = new ByteArrayOutputStream();
+          ObjectOutputStream oos = new ObjectOutputStream(bos)
+      ) {
+        oos.writeObject(value);
+
+        final byte[] valueAsBytes = bos.toByteArray();
+
+        System.out.printf("%s -- value %s\n", key, Arrays.toString(valueAsBytes));
+      }
+    }
+  }
+
+  private static String getInfoHash(Map<String, Object> torrentFileDecoded) {
+    final byte[] encodedInfo = bencodeForTorrentFile.encode((Map<?, ?>) torrentFileDecoded.get("info"));
+
+    final String infoHash = getHashedValue(encodedInfo);
+
+    return infoHash;
   }
 
   static String decodeBencode(String bencodedString) {
@@ -58,6 +100,25 @@ public class Main {
     } else {
       throw new RuntimeException("Only strings are supported at the moment");
     }
+  }
+
+  public static String getHashedValue(byte[] input) {
+    try {
+      MessageDigest md = MessageDigest.getInstance("SHA-1");
+      md.update(input);
+      byte[] digest = md.digest();
+
+      StringBuilder hexString = new StringBuilder();
+
+      for (byte b : digest) {
+        hexString.append(String.format("%02x", b));
+      }
+
+      return hexString.toString();
+    } catch (NoSuchAlgorithmException e) {
+      throw new RuntimeException(e);
+    }
+
   }
 
 }
